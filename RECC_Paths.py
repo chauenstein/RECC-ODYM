7e5f9bf481bad48118e4089778fe18aa5ecbd674
Marcel Geller <marcelgeller@gmx.de>
2026-04-16 18:21:57 +0200

    Integrated pv subtechnologies via market shares. 2023 modeled stock = 2023 ESM reported stock

---
+++ RECC_Paths.py
@@ -0,0 +1,11 @@
+### Start of path file ###
+### These paths are for Marcels Mac machine ###
+
+odym_path =         '/Users/marcelgeller/Work/RECC/RECC_Model/ODYM_old'
+data_path =         '/Users/marcelgeller/Work/RECC/RECC_Model/RECC_Database/CURRENT_V2_5'
+results_path =      '/Users/marcelgeller/Work/RECC/RECC_Model/RECC_Results'
+#results_path_eval = '/Users/marcelgeller/Work/RECC/RECC_Model/Evaluate'
+recc_path =         '/Users/marcelgeller/Work/RECC/RECC_Model/RECC-ODYM'
+#export_path = '/Users/marcelgeller/Work/RECC/RECC_Model/Data_Export'
+
+### End of path file ###
